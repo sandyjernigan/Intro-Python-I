00b7601090bf3ea32883074056c952dd19b66211
Sandy <sandy.jernigan@gmail.com>
2020-02-17 01:47:44 -0500

    Update 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -4,16 +4,25 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+def f1(x,y):
+    return x + y
 
+print("Function 1: ")
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+def f2(*argv):
+    if type(argv[0]) == int:
+        return sum(argv)
+    elif type(argv[0]) == list: 
+        return sum(argv[0])
+    else:
+        return type(argv)
 
+print("Function 2: ")
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -29,8 +38,10 @@ print(f2(a))    # Should print 22
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+def f3( x, y = 1 ):
+    return x + y
 
+print("Function 3: ")
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -43,7 +54,14 @@ print(f3(8))     # Should print 9
 #
 # Note: Google "python keyword arguments".
 
-# YOUR CODE HERE
+def f4(a=None,**kwargs):
+    if type(a) == dict:
+        print(a)
+    else:
+        for key, value in kwargs.items():
+            print(f"key: {key}, value: {value}")
+
+print("Function 4: ")
 
 # Should print
 # key: a, value: 12
