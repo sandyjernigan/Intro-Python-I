c573d1708ebda0f936f027960406ce8008992ca0
Sandy <sandy.jernigan@gmail.com>
2020-02-18 19:56:06 -0500

    Update 14_cal.py

--- src/14_cal.py
+++ src/14_cal.py
@@ -29,4 +29,24 @@ it should use today’s date to get the month and year.
 
 import sys
 import calendar
-from datetime import datetime
+from datetime import datetime, date
+
+# Get Today's Date
+dt = date.today()
+
+# Set Calendar Year
+cy = dt.year
+# Set Calendar Month
+cm = dt.month
+
+user_input = "" #input("Pick a Date: ")
+
+c = calendar.TextCalendar(calendar.SUNDAY)
+
+# Print Calendar
+printCalendar = c.formatmonth(cy, cm)
+print(printCalendar)
+
+
+if user_input == "":
+  selectedDate = "today"
